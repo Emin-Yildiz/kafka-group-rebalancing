812f119b0676ae6d1734690073e344d4c0d37efd
Emin_Yildiz <eminyildiz122@gmail.com>
2025-10-06 21:55:47 +0300

    project started

---
+++ src/main/java/org/example/kafkagrouprebalancing/KafkaGroupRebalancingApplication.java
@@ -0,0 +1,13 @@
+package org.example.kafkagrouprebalancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaGroupRebalancingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaGroupRebalancingApplication.class, args);
+    }
+
+}
